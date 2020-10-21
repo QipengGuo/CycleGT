9d7b7d9e4f4fe1b4e598c8a0b6a4c57188242958
zhijing <zhijing@mit.edu>
2020-10-21 05:03:32 -0400

    added plot of loss

---
+++ visualization/plot_loss.py
@@ -0,0 +1,30 @@
+import numpy as np
+import seaborn as sns
+
+file = 'temp.npz'
+a = np.load(file)
+
+num = 20
+loss1 = a['loss1']
+loss2 = a['loss2']
+
+def plot(loss1, loss2):
+    sns.set(font_scale=1.5)
+
+    data = {
+        'epoch': list(range(len(loss1[::num]))) + list(range(len(loss2[::num]))),
+        'loss': list(loss1[::num]) + list(loss2[::num]),
+        'model': ['Base'] * len(loss1[::num]) + ['CVAE'] * len(loss2[::num]),
+    }
+    printout = {k: len(v) for k, v in data.items() }
+    print('check length:', printout)
+    plot = sns.lineplot(data=data, x='epoch', y='loss', hue="model",
+                 )
+    plot.set(ylim=(0, 0.4), xlabel='Epoch', ylabel="Reconstruction Error",)
+
+    import matplotlib.pyplot as plt
+    plt.tight_layout()
+    plt.show()
+
+plot(loss1, loss2)
+
